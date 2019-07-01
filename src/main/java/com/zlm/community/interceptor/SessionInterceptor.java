8fbf3be0698644671cf097532c55840cc76225c0
zlmgit <1552005882@qq.com>
2019-07-01 08:01:44 +0800

    提交

---
+++ src/main/java/com/zlm/community/interceptor/SessionInterceptor.java
@@ -0,0 +1,50 @@
+package com.zlm.community.interceptor;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zlm.community.model.User;
+import com.zlm.community.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Service
+public class SessionInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private IUserService userService;
+
+    @Value("${community.token}")
+    private String     communityToken;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (communityToken.equals(cookie.getName())) {
+                    User user = userService.getOne(new QueryWrapper<User>().eq("token",cookie.getValue()));
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/zlm/community/interceptor/WebConfig.java
@@ -0,0 +1,16 @@
+package com.zlm.community.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**").excludePathPatterns("/admin/**");
+    }
+}
