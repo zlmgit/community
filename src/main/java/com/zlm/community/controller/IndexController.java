3c31fbb9d0a42e3f295ec4b8ae3d9182dc122ee5
zlmgit <1552005882@qq.com>
2019-07-04 07:50:12 +0800

    提交

--- src/main/java/com/zlm/community/controller/IndexController.java
+++ src/main/java/com/zlm/community/controller/IndexController.java
@@ -1,24 +1,15 @@
 package com.zlm.community.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zlm.community.dto.PaginationDTO;
 import com.zlm.community.dto.QuestionDTO;
-import com.zlm.community.model.Question;
-import com.zlm.community.model.User;
 import com.zlm.community.service.IQuestionService;
-import com.zlm.community.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
